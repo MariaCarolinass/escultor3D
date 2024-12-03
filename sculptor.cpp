f5f99b91b0905ac5d0ba207a9e97f6c1b84345ec
mariacarilinass <m.carolina.soares1@gmail.com>
2024-12-03 13:20:59 -0300

    implementei construtor e destrutor

--- sculptor.cpp
+++ sculptor.cpp
@@ -1,20 +1,56 @@
 #include "sculptor.h"
 
-Sculptor::Sculptor() {}
-
 Sculptor::Sculptor(int _nx, int _ny, int _nz)
 {
-    nx = _nx;
-    ny = _ny;
-    nz = _nz;
+    nx = _nx; // planos
+    ny = _ny; // linhas
+    nz = _nz; // colunas
+    
+    // alocação de memória do primeiro ponteiro 
+    v = new Voxel **[nx];
+
+    for (int i = 0; i < nx; i++) {
+        // alocação do segundo ponteiro
+        v[i] = new Voxel *[ny];
+    
+        for (int j = 0; j < ny; j++) {
+            // alocação do terceiro ponteiro
+            v[i][j] = new Voxel[nz];
+            
+            // defindo variáveis com valor padrão 
+            for (int k = 0; k < nz; k++) {
+                v[i][j][k].r = 0; 
+                v[i][j][k].g = 0;
+                v[i][j][k].b = 0;
+                v[i][j][k].a = 0;
+                v[i][j][k].show = false;
+            }
+        }
+    }
 }
 
 Sculptor::~Sculptor()
 {
+    // liberação de memória dos ponteiros
+    for (int i = 0; i < nx; i++) {
+        for (int j = 0; j < ny; j++) {
+            delete[] v[i][j]; // libera o terceiro ponteiro
+        }
+        delete[] v[i]; // libera o segundo ponteiro
+    }
+    delete[] v; // libera o primeiro ponteiro
 
+    nx = 0;
+    ny = 0;
+    nz = 0;
+
+    r = 0; 
+    g = 0;
+    b = 0;
+    a = 0;
 }
 
-void Sculptor::setColor(float r, float g, float b, float a)
+void Sculptor::setColor(float _r, float _g, float _b, float _a)
 {
 
 }
