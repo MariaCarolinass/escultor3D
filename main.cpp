95cbaf3dbb59140657f612f02b22889207239d33
mariacarilinass <m.carolina.soares1@gmail.com>
2024-12-03 13:23:20 -0300

    chamei métodos do Sculptor

--- main.cpp
+++ main.cpp
@@ -2,7 +2,10 @@
 
 int main()
 {
-    Sculptor sculptor();
+    Sculptor sculptor(6, 6, 6);
+    sculptor.setColor(255, 0, 0, 1.0);
+    sculptor.putVoxel(2, 3, 1);
+    sculptor.cutVoxel(2, 3, 1);
 
     return 0;
 }
