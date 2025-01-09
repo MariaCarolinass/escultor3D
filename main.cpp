610173d2c4931078bc05d9d7ea69f7e2b6709394
mariacarolinass <m.carolina.soares1@gmail.com>
2025-01-08 23:04:40 -0300

    chamei método para criar figura

--- main.cpp
+++ main.cpp
@@ -1,24 +1,11 @@
 #include "sculptor.h"
 #include "readfile.h"
-#include <iostream>
 
 using namespace std;
 
 int main() {
-    const int SIZE = 100;
-    char input[SIZE];
-
-    cout << "Digite o nome do arquivo: ";
-    cin.getline(input, SIZE);
-
-    if (input[0] == '\0') {
-        cout << "Erro: Nenhum nome foi digitado." << endl;
-    } else {
-        Sculptor *objSculptor(ReadFile(input).objSculptor);
-        objSculptor->writeVECT(input);
-        objSculptor->writeOFF(input);
-        cout << "Figura criada com sucesso!" << endl;
-    }
+    Sculptor *objSculptor(ReadFile("/home/oem/Cpp-Projects/escultor3D/testeDesenhos/trono.txt").objSculptor);
+    objSculptor->writeOFF("/home/oem/Cpp-Projects/escultor3D/testeDesenhos/trono.off");
 
     return 0;
 }
